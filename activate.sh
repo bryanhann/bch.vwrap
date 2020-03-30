d9ef4c97090852ca891cc51e93a204f3bb8d0e6d
Bryan Hann <nobody@nowhere>
2020-03-30 16:45:20 +0800

    Add installation flag; remove env variable definitions

--- activate.sh
+++ activate.sh
@@ -1,7 +1,8 @@
-here=$(dirname $(grealpath $0))
-pip install --cache-dir $PIP_DOWNLOAD_CACHE virtualenv
-pip install --cache-dir $PIP_DOWNLOAD_CACHE virtualenvwrapper
-export VIRTUALENVWRAPPER_HOOK_DIR  ${here}/hooks
-export WORKON_HOME                 ${here}/workon_home
-export PROJECT_HOME                ~/dev
+H=$(dirname $(grealpath $0))
+flag=${H}/flag.installed
+[ -f $flag ] || {
+    pip install --cache-dir $PIP_DOWNLOAD_CACHE virtualenv
+    pip install --cache-dir $PIP_DOWNLOAD_CACHE virtualenvwrapper
+    touch $flag
+}
 source  virtualenvwrapper.sh
