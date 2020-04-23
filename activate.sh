8b2a195ca928c6ca19d3b148ee2129120503e2e9
Bryan Hann <nobody@nowhere>
2020-04-23 18:54:01 +0800

    Skip flag. Try to "pip install" every time.

--- activate.sh
+++ activate.sh
@@ -1,8 +1,3 @@
-H=$(dirname $(grealpath $0))
-flag=${H}/flag.installed
-[ -f $flag ] || {
-    pip install --cache-dir $PIP_DOWNLOAD_CACHE virtualenv
-    pip install --cache-dir $PIP_DOWNLOAD_CACHE virtualenvwrapper
-    touch $flag
-}
+pip install virtualenv
+pip install virtualenvwrapper
 source  virtualenvwrapper.sh
